488c31982d4f8b17a7107dfff9d5a0089f863935
xichuanwushao <avatarwx@163.com>
2022-03-17 21:17:38 +0800

    3.1.2 添加日志打印

--- xichuan-news-dev-service-user/src/main/java/com/xichuan/user/controller/HelloController.java
+++ xichuan-news-dev-service-user/src/main/java/com/xichuan/user/controller/HelloController.java
@@ -1,11 +1,20 @@
 package com.xichuan.user.controller;
 
 import com.xichuan.api.controller.HelloControllerApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController implements HelloControllerApi {
+
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @Override
     public Object hello() {
+        logger.debug("debug: hello~");
+        logger.info("info: hello~");
+        logger.warn("warn: hello~");
+        logger.error("error: hello~");
         return "hello";
     }
 

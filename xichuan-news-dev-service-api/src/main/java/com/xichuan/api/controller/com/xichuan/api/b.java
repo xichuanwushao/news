0b2386907e97c8f3ec6df6f417ea7f6182aadd4c
xichuanwushao <avatarwx@163.com>
2022-03-19 15:07:24 +0800

    3.1.5 将pojo 中的 fans user和mapper移动到指定位置

--- xichuan-news-dev-service-api/src/main/java/com/xichuan/api/controller/com/xichuan/api/b.java
+++
@@ -1,8 +0,0 @@
-package com.xichuan.api.controller.com.xichuan.api;
-
-/**
- * @author : wuxiao
- * @date : 9:49 2022/3/19
- */
-public class b {
-}
---
+++ xichuan-news-dev-service-user/src/main/java/com/xichuan/user/mapper/AppUserMapper.java
@@ -0,0 +1,16 @@
+package com.xichuan.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xichuan.model.pojo.AppUser;
+
+/**
+ * <p>
+ * 网站用户 Mapper 接口
+ * </p>
+ *
+ * @author wuxiao
+ * @since 2022-03-19
+ */
+public interface AppUserMapper extends BaseMapper<AppUser> {
+
+}

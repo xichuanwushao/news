ad5a42f18f5def975b84486b92ec9fefb9d41a19
xichuanwushao <avatarwx@163.com>
2022-04-07 16:59:54 +0800

    3-28 退出登录&注销会话

--- xichuan-news-dev-service-api/src/main/java/com/xichuan/api/BaseController.java
+++ xichuan-news-dev-service-api/src/main/java/com/xichuan/api/BaseController.java
@@ -29,6 +29,7 @@ public class BaseController {
     public String DOMAIN_NAME;
     public static final Integer COOKIE_MONTH = 30 * 24 * 60 * 60;
 
+    public static final Integer COOKIE_DELETE = 0;
     /**
      * 获取BO中的错误信息
      * @param result
--- xichuan-news-dev-service-user/src/main/java/com/xichuan/user/controller/PassportController.java
+++ xichuan-news-dev-service-user/src/main/java/com/xichuan/user/controller/PassportController.java
@@ -104,4 +104,17 @@ public class PassportController extends BaseController implements PassportContro
         // 5. 返回用户状态
         return GraceJSONResult.ok(userActiveStatus);
     }
+
+    @Override
+    public GraceJSONResult logout(String userId,
+                                  HttpServletRequest request,
+                                  HttpServletResponse response) {
+
+        redis.del(REDIS_USER_TOKEN + ":" + userId);
+
+        setCookie(request, response, "utoken", "", COOKIE_DELETE);
+        setCookie(request, response, "uid", "", COOKIE_DELETE);
+
+        return GraceJSONResult.ok();
+    }
 }
